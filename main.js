bfe6daed7f55d1dc2899189f5d5dd50f4feb9b09
lam7239 <nklam7239@gmail.com>
2025-05-01 13:51:46 +0700

    Add main.js 1746082302330

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+app.commandLine.appendSwitch('no-sandbox');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+  // win.webContents.openDevTools(); // Uncomment to open dev tools
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
